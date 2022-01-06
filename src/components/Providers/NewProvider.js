bdf2710a09a5dbbfbcbade0e2b32d8405133b2ff
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2022-01-05 22:18:51 -0500

    working on login

--- src/components/Providers/NewProvider.js
+++ src/components/Providers/NewProvider.js
@@ -1,16 +1,36 @@
-import React, {useState, useEffect} from 'react';
 import axios from 'axios';
+import React, {useState, useEffect} from 'react';
 
 const NewsContext = React.createContext({});
 
-const NewsProvider =(props)=>{
+const NewsProvider = (props) => {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [query, setQuery] = useState("javascript");
 
-    const {articles,loading}= useState([]);
 
+  useEffect(() => {
+    // get the news from the api.
+    const getNews = async (query) => {
+      try {
+        const res = await axios.get(`http://localhost:8080/api/test/news/${query}`)
+        setArticles(res.data);
+        setLoading(false);
+      } catch (err) {
+        console.error(err.message);
+      }
+    }
+    setLoading(true);
+    getNews(query);
 
-        const displayAtricles=()=>{
-            return articles.map((article, i)=>(
-                <Article article={article} key={article.url}/>
-            ))
-        }
+  }, [query])
+
+  // {articles, loading} == {articles: articles, loading: loading}
+  return (
+    <NewsContext.Provider value={{articles, loading, setQuery}}> 
+      {props.children}
+    </NewsContext.Provider>
+  )
 }
+
+export {NewsProvider, NewsContext};
